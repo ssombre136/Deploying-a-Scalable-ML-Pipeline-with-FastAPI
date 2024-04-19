7a99de5f93c22d239dd7967940af863753f5d80e
Rowan Zinn <rowanzinn@gmail.com>
2024-04-19 13:49:36 -0600

    Trying to make the action run

--- ml/data.py
+++ ml/data.py
@@ -54,7 +54,7 @@ def process_data(
     X_continuous = X.drop(*[categorical_features], axis=1)
 
     if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
+        encoder = OneHotEncoder(handle_unknown="ignore")
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
         y = lb.fit_transform(y.values).ravel()
@@ -66,7 +66,7 @@ def process_data(
         except AttributeError:
             pass
 
-    X = np.concatenate([X_continuous, X_categorical], axis=1)
+    X = np.concatenate([X_continuous, X_categorical.toarray()], axis=1)
     return X, y, encoder, lb
 
 def apply_label(inference):

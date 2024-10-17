30935e79fa1ad000f96eacb9d3b14919ac1aba3e
root <root@DESKTOP-D7RECIN>
2024-10-16 15:36:35 -1000

    Steps 1 - 4 completed

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,49 @@
 import pytest
 # TODO: add necessary import
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from ml.model import train_model, compute_model_metrics
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_model_returns_randomforestclassifier():
     """
-    # add description for the first test
+    Using dummy variables tests if the training model returns a RandomForestClassifier
     """
     # Your code here
+    row_train = np.random.rand(20, 5)
+    col_train = np.random.randint(1, 2, size=20)
+    model = train_model(row_train, col_train)
+    assert isinstance(model, RandomForestClassifier)
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_algorithm_randomforestclassifier():
     """
-    # add description for the second test
+    Using dummy variables tests if model is using RandomForestClassifier for the algorithm
     """
     # Your code here
+    row_train = np.random.rand(20, 5)
+    col_train = np.random.randint(1, 2, size=20)
+    model = train_model(row_train, col_train)
+    assert model.__class__ is RandomForestClassifier
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_model_metrics():
     """
-    # add description for the third test
+    Using dummy variables verifies that the model's accuracy metric is as expected.
     """
     # Your code here
+    row_train = np.random.rand(20, 5)
+    col_train = np.random.randint(1, 2, size=20)
+    row_test = np.random.rand(20, 5)
+    col_test = np.random.randint(1, 2, size=20)
+    model = train_model(row_train, col_train)
+    prediction = model.predict(row_test)
+    accuracy = compute_model_metrics(col_test, prediction)
+    assert np.all(np.isclose(accuracy, accuracy_score(col_test, prediction)))
+    
     pass
